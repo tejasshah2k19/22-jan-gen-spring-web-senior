268b288bb752a9473204a16997c9a33f6900209a
Tejas <tejas14all@gmail.com>
2022-02-01 17:56:25 +0530

    web

--- src/main/java/com/controller/UserController.java
+++ src/main/java/com/controller/UserController.java
@@ -1,29 +1,32 @@
 package com.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.bean.UserBean;
 
 @Controller
 public class UserController {
 
-	
-	@RequestMapping("/login")
-	void login() {
-		System.out.println("login");
+	@RequestMapping(value = "/signup", method = RequestMethod.GET)
+//	@RequestMapping("/signup")//GET 
+	public String signup() {
+		System.out.println("hello signup......");
+		return "Signup";// jsp
 	}
 
-	@RequestMapping("/signup")	
-	void signup() {
-
-	}
-
-	
-	
-	void logout() {
-
-	}
-
-	void forgetPassword() {
-
+	@RequestMapping(value = "saveuser", method = RequestMethod.POST)
+	public String saveUser(UserBean user, Model model) {
+		// request
+		// bean
+		// bean.set
+		// dao.insert(bean)
+		System.out.println("Save user () ");
+		System.out.println(user.getFirstName());
+		System.out.println(user.getEmail());
+		model.addAttribute("user", user);
+		return "Home";
 	}
 }
---
+++ src/main/webapp/WEB-INF/views/Signup.jsp
@@ -0,0 +1,26 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+</head>
+<body>
+
+	<h2>Signup</h2>
+
+
+
+	<form action="saveuser" method="post">
+
+		FirstName : <input type="text" name="firstName" /> <br> <BR>
+		Email : <input type="text" name="email" /><br> <BR>
+		Password : <input type="text" name="password" /> <br> <BR>
+
+		<input type="submit" value="Signup" />
+
+
+	</form>
+
+</body>
+</html>
