7eca6f683255d994f333acc48e3e14fe705b9f34
Tejas <tejas14all@gmail.com>
2022-02-15 17:51:18 +0530

    dbconnection

---
+++ src/main/java/com/bean/CustomerBean.java
@@ -0,0 +1,42 @@
+package com.bean;
+
+public class CustomerBean {
+
+	int customerId;
+	String firstName;
+	String password;
+	String email;
+	int balance;
+	public int getCustomerId() {
+		return customerId;
+	}
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getBalance() {
+		return balance;
+	}
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+	
+	
+}
